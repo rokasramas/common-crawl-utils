ca594ff3893aca92a41e5927320bec9842aebc46
Rokas Ramanauskas <rokasramas@gmail.com>
2022-04-15 16:55:08 +0300

    Update cc base url

--- src/common_crawl_utils/constants.clj
+++ src/common_crawl_utils/constants.clj
@@ -2,6 +2,6 @@
 
 (def index-collinfo "http://index.commoncrawl.org/collinfo.json")
 
-(def cc-s3-base-url "https://commoncrawl.s3.amazonaws.com/")
+(def cc-s3-base-url "https://data.commoncrawl.org/")
 
 (def http-timeout 10000)
